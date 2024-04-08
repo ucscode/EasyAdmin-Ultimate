5912b385dd61eb01b65893a60ed63d2ae24fd18b
ucscode <uche23mail@gmail.com>
2024-04-08 11:11:44 +0100

    feat: Admin interface created

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin/index.html.twig');
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
