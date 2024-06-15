f6f45eea3fef31445312a08e1d8480c6fcceee20
ucscode <uche23mail@gmail.com>
2024-06-15 21:31:34 +0100

    feat: Added product abstraction

---
+++ src/Entity/Abstracts/AbstractProduct.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Entity\Abstracts;
+
+use App\Entity\Media;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+abstract class AbstractProduct
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    protected ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    protected ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    protected ?string $description = null;
+    
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    protected ?Media $image = null;
+
+    #[ORM\Column]
+    protected ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    protected ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column(length: 20)]
+    protected ?string $status = null;
+
+    #[ORM\Column]
+    protected ?int $originalPrice = null;
+
+    #[ORM\Column]
+    protected ?int $salePrice = null;
+
+    #[ORM\Column(length: 20)]
+    protected ?string $sku = null;
+
+    public function __construct()
+    {
+        $this->updatedAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?Media
+    {
+        return $this->image;
+    }
+
+    public function setImage(Media $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getSalePrice(): ?int
+    {
+        return $this->salePrice;
+    }
+
+    public function setSalePrice(int $price): static
+    {
+        $this->salePrice = $price;
+
+        return $this;
+    }
+
+    public function getOriginalPrice(): ?int
+    {
+        return $this->originalPrice;
+    }
+
+    public function setOriginalPrice(int $originalPrice): static
+    {
+        $this->originalPrice = $originalPrice;
+
+        return $this;
+    }
+
+    public function getSku(): ?string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(string $sku): static
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+}
