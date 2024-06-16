f16b6e7ae59c8c8ce2a5f78ed8e868fb8d032724
ucscode <uche23mail@gmail.com>
2024-06-16 21:53:23 +0100

    fix: Property Update

--- src/Entity/User/Property.php
+++ src/Entity/User/Property.php
@@ -11,9 +11,19 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 #[UniqueEntity(fields: ['user', 'metaKey'])]
 class Property extends AbstractMeta
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    protected ?int $id = null;
+
     #[ORM\ManyToOne(inversedBy: 'properties')]
     #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
     private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
     public function getUser(): ?User
     {
