a59395e0df5596c8bf0ff67615b7d0ceb66f33ef
ucscode <uche23mail@gmail.com>
2024-07-23 21:53:58 +0100

    feat: sort entities compactably

--- src/Controller/Initial/Abstracts/AbstractInitialCrudController.php
+++ src/Controller/Initial/Abstracts/AbstractInitialCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Initial\Abstracts;
 
 use App\Controller\Initial\Traits\InitialControllerTrait;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 /**
@@ -17,4 +21,45 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 abstract class AbstractInitialCrudController extends AbstractCrudController
 {
     use InitialControllerTrait;
+
+    protected function disableAllActions(Actions $actions, string|array $exempt = []): Actions
+    {
+        $defaults = [
+            Action::NEW,
+            Action::DELETE,
+            Action::DETAIL,
+            Action::EDIT,
+        ];
+
+        !is_string($exempt) ?: $exception = [$exempt];
+
+        foreach(array_diff($defaults, $exception) as $action) {
+            $actions->disable($action);
+        }
+
+        return  $actions;
+    }
+    
+    protected function onlyUserEntities(QueryBuilder $queryBuilder): QueryBuilder
+    {
+        return $queryBuilder
+            ->andWhere('entity.user = :user')
+            ->setParameter('user', $this->getUser())
+        ;
+    }
+
+    /**
+     * @param array<string,string> $orderBy Sort an entity by [property => ASC|DESC] 
+     */
+    protected function sortIndexQueryEntities(QueryBuilder $queryBuilder, array $orderBy = ['id' => 'DESC']): QueryBuilder
+    {
+        /**
+         * @var \Symfony\Component\HttpFoundation\Request $request
+         */
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        
+        $criteria = Criteria::create()->orderBy(array_merge($orderBy, $request->query->all('sort')));
+
+        return $queryBuilder->addCriteria($criteria);
+    }
 }
